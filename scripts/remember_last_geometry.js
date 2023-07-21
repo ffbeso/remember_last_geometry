19e365db6e2e4ff9d24588aeda21a4754ef407a8
Sho Nitta <besokko@gmail.com>
2023-07-22 00:00:47 +0900

    minimal fix

--- scripts/remember_last_geometry.js
+++ scripts/remember_last_geometry.js
@@ -1,6 +1,6 @@
 // Sometime the leftmost screen doesn't have id 0
 // not sure yet how to detect this automatically
-var leftMostScreen = 2
+var leftMostScreen = 0
 
 // Some setup used by both reading and writing
 var dir = mp.utils.split_path(mp.get_script_file())[0]
@@ -29,7 +29,7 @@ catch (e) {
 // Save the rect at shutdown
 function save_rect() {
     var ps1_script = mp.utils.join_path(dir.replace("s/", "-opts/"), scriptName + ".ps1")
-    var output = mp.utils.subprocess({ args: ["powershell", ps1_script + " " + mp.utils.getpid()], cancellable: false }).stdout
+    var output = mp.utils.subprocess({ args: ["powershell", ps1_script, mp.utils.getpid()], cancellable: false }).stdout
     mp.utils.write_file("file://" + dir.replace("s/", "-opts/") + scriptName + ".conf", "geometry=" + output)
 }
 mp.register_event("shutdown", save_rect)
