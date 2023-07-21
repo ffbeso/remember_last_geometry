3b159e18b9e92510fd545378b8995cabfd5ac07f
Sho Nitta <besokko@gmail.com>
2023-07-22 00:30:41 +0900

    fix

--- scripts/remember_last_geometry.js
+++ scripts/remember_last_geometry.js
@@ -7,17 +7,11 @@ var dir = mp.utils.split_path(mp.get_script_file())[0]
 var scriptName = mp.get_script_name()
 
 var o = {geometry : ""}
-var option = mp.options
-option.read_options(o)
+mp.options.read_options(o)
 
 // Read last window rect if present
 try {
-    var rect = o.geometry.trim().split(' ')
-
-    var x = rect[0]
-    var y = rect[1]
-    var width = rect[2]
-    var height = rect[3]
+    var {x, y, width, height} = o.geometry.trim().split(' ')
     mp.set_property("screen", leftMostScreen)
     var geometry = width + "x" + height + "+" + x + "+" + y
     mp.set_property("geometry", geometry)
@@ -28,7 +22,7 @@ catch (e) {
 
 // Save the rect at shutdown
 function save_rect() {
-    var ps1_script = mp.utils.join_path(dir.replace("s/", "-opts/"), scriptName + ".ps1")
+    var ps1_script = mp.utils.join_path(dir.replace("scripts/", "script-opts/"), scriptName + ".ps1")
     var output = mp.utils.subprocess({ args: ["powershell", ps1_script, mp.utils.getpid()], cancellable: false }).stdout
     mp.utils.write_file("file://" + dir.replace("s/", "-opts/") + scriptName + ".conf", "geometry=" + output)
 }
